e8f35a33183f69408d13933c85324d0d17b63a6d
Patrik Marin <patrik.marin@qoco.fi>
2020-09-24 18:58:39 +0300

    Add another handler

--- codebase/src/handler/index.ts
+++ codebase/src/handler/index.ts
@@ -1,6 +1,35 @@
 import { LambdaEvent, LambdaResponse } from "types";
 
-import { successResponse } from "../util";
+import { successResponse, generateDateString } from "../util";
 
 export const ping = async (event: LambdaEvent): LambdaResponse<string> =>
   successResponse("pong");
+
+interface AppInformation {
+  metrics: {
+    lastFetched: string;
+    lastMigrated: string;
+    totalEntries: number;
+  };
+  status: {
+    entriesWaiting: number;
+    nextFetchIn: string;
+    nextMigrationIn: string;
+  };
+}
+
+export const getMetrics = async (
+  event: LambdaEvent
+): LambdaResponse<AppInformation> => {
+  const metrics: AppInformation["metrics"] = {
+    lastFetched: generateDateString(Date.now() - 100000),
+    lastMigrated: generateDateString(Date.now() - 2000000),
+    totalEntries: 10,
+  };
+  const status: AppInformation["status"] = {
+    entriesWaiting: 10,
+    nextFetchIn: generateDateString(Date.now() + 20000),
+    nextMigrationIn: generateDateString(Date.now() + 500000),
+  };
+  return successResponse({ metrics, status });
+};
--- codebase/src/util/index.ts
+++ codebase/src/util/index.ts
@@ -1,16 +1,23 @@
-import { LambdaSuccessResponse } from 'types';
+import { LambdaSuccessResponse } from "types";
+import formatDistance from "date-fns/formatDistance";
+import formatISO from "date-fns/formatISO";
+
+export const generateDateString = (date: Date | number) => {
+  const readable = formatDistance(date, Date.now(), { addSuffix: true });
+  return `${formatISO(date)} (${readable})`;
+};
 
 export const successResponse = <T extends Record<string, any> | null | string>(
-  body: T,
+  body: T
 ): Promise<LambdaSuccessResponse<T>> =>
   Promise.resolve({
     statusCode: 200,
     body:
-      typeof body === 'string' || body === null
+      typeof body === "string" || body === null
         ? (body as string | null)
         : JSON.stringify(body),
     headers: {
-      'Content-Type':
-        typeof body === 'string' ? 'text/plain' : 'application/json',
+      "Content-Type":
+        typeof body === "string" ? "text/plain" : "application/json",
     },
   });
