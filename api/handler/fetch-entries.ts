4a580707b601f1f909399d3060996313b3b7ced8
Patrik Marin <patrik.marin@qoco.fi>
2020-09-26 01:05:45 +0300

    Parse entries

--- api/handler/fetch-entries.ts
+++ api/handler/fetch-entries.ts
@@ -8,9 +8,44 @@ import { LambdaEvent, LambdaResponse } from 'service/types';
 import { TimeEntryResponse } from 'toggl-api/types';
 
 interface FetchLatestEntriesResponse {
-  sourceEntries: TimeEntryResponse[];
-  targetEntries: TimeEntryResponse[];
+  existingEntries: TimeEntryResponse[];
+  newEntries: TimeEntryResponse[];
+  deletedEntries: TimeEntryResponse[];
 }
+
+const parseEntries = async ({
+  sourceEntries,
+  targetEntries,
+}: Record<'sourceEntries' | 'targetEntries', TimeEntryResponse[]>) => {
+  const iteratee = ({ start, duration }: TimeEntryResponse) =>
+    `${start}${duration}`;
+
+  const sourceList = new Set(sourceEntries.map(iteratee));
+  const targetList = new Set(targetEntries.map(iteratee));
+  const intersectionList = new Set(
+    [...sourceList].filter((x) => targetList.has(x)),
+  );
+
+  const [newEntries, deletedEntries, existingEntries] = await Promise.all([
+    Promise.resolve(
+      sourceEntries.filter((x) => !intersectionList.has(iteratee(x))),
+    ),
+    Promise.resolve(
+      targetEntries.filter((x) => !intersectionList.has(iteratee(x))),
+    ),
+    Promise.resolve(
+      [...sourceEntries, ...targetEntries].filter((x) =>
+        intersectionList.has(iteratee(x)),
+      ),
+    ),
+  ]);
+
+  return {
+    existingEntries,
+    newEntries,
+    deletedEntries,
+  };
+};
 
 export const fetchLatestEntries = async (
   event: LambdaEvent,
@@ -27,12 +62,14 @@ export const fetchLatestEntries = async (
       fetchLatestTogglEntries({
         apiToken: ssmValues[name],
         days: 3,
-      }),
+      }).then((entries) => entries.filter((entry) => entry.duration > 0)),
     ),
   );
 
-  return successResponse({
+  const parsedEntries = await parseEntries({
     sourceEntries,
     targetEntries,
   });
+
+  return successResponse(parsedEntries);
 };
